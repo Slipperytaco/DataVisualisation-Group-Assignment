21ee7c0a473bc982d9b38bc3b22d7c8f6a0570ed
Ari <74815805+Slipperytaco@users.noreply.github.com>
2023-05-08 14:00:42 +1000

    added some source data in csv format

--- script/indexScript.js
+++ script/indexScript.js
@@ -1,5 +1,19 @@
 function init(){
+    //initailisation of variables 
+    var w = 1000; 
+    var h = 1000; 
+    var padding = 25; 
     
+    //selecting chart 
+    var svg = d3.select("#chart")
+            .append("svg")
+            .attr("width", w + padding)
+            .attr("height", h + padding);
 
-}
-window.onload = init;
+    //importing of data from csv
+    d3.csv("resources\netMigrationCalendarYear2004-2019.csv").then(function(data) {
+        console.log(data)
+    });
+
+};
+window.onload = init
